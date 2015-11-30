c618e4e7558f19f690d4a94723321f07b8c48806
Jason Frost <jasonf@hotmail.com.au>
2015-11-30 11:17:29 +1100

    added sql to create tables

---
+++ setup/dbsetup.inc.php
@@ -0,0 +1,38 @@
+<?php
+$user_login_table = "
+CREATE TABLE user_login (
+  login_id int AUTO_INCREMENT,
+  password binary(60) NOT NULL,
+  email_address varchar(254) NOT NULL,
+  PRIMARY KEY (login_id)
+) CHARACTER SET=utf8mb4";
+
+$user_info_table = "
+CREATE TABLE user_info (
+  user_id int AUTO_INCREMENT,
+  login_id int NOT NULL,
+  first_name varchar(40),
+  other_name varchar(40),
+  last_name varchar(40),
+  display_name varchar(122),
+  PRIMARY KEY (user_id),
+  FOREIGN KEY (login_id) REFERENCES user_login(login_id)
+) CHARACTER SET=utf8mb4";
+  
+$post_table = "
+CREATE TABLE posts (
+  post_id int AUTO_INCREMENT,
+  created_time datetime NOT NULL,
+  modified_time datetime NOT NULL,
+  author_id int NOT NULL,
+  body text NOT NULL,
+  PRIMARY KEY (post_id),
+  FOREIGN KEY (author_id) REFERENCES user_info(user_id)
+) CHARACTER SET=utf8mb4";
+
+$mysql_queries = array(
+  $user_login_table,
+  $user_info_table,
+  $post_table,
+);
+?>
