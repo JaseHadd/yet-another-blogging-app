67fe8a35009a745f45b94a0864fa3553c53574c8
Jason Frost <jasonf@hotmail.com.au>
2015-12-17 01:47:39 +1100

    fixed object property names

--- includes/pdo.inc.php
+++ includes/pdo.inc.php
@@ -6,8 +6,8 @@ include_once('files.inc.php');
 
 function database_connect() {
   $db = load_object('db');
-  $dsn = "{$db->db_driver}:host={$db->db_host};dbname={$db->db_database};charset=utf8";
-  $link = new PDO($dsn, $db_username, $db_password);
+  $dsn = "{$db->driver}:host={$db->host};dbname={$db->database};charset=utf8";
+  $link = new PDO($dsn, $db->username, $db->password);
   
   return $link;
 }
