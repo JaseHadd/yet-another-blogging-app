6f21f31ae0ed0cf1008ca07c2807a8b65584fb1a
Jason Frost <jasonf@hotmail.com.au>
2015-12-17 01:17:32 +1100

    added get_session_array and finished login page

--- includes/user.inc.php
+++ includes/user.inc.php
@@ -13,7 +13,7 @@ function login($email_address, $password) {
   $link = database_connect();
   $query = "SELECT login_id, password FROM {$db->prefix}user_login WHERE email_address = :email LIMIT 1";
   $statement = $link->prepare($query);
-  $statement->bindParam(':email', $email_address);
+  $statement->bindParam('email', $email_address);
   $statement->execute();
   
   $result = $statement->fetch(PDO::FETCH_OBJ);
@@ -21,7 +21,19 @@ function login($email_address, $password) {
     return $result->login_id;
   }
   return 0;
+}
+
+function get_session_array($login_id) {
+  $session = ['login_id' => $login_id];
+  $config = load_object('db');
+  $link = database_connect();
+  $query = "SELECT first_name, last_name, display_name FROM {$db->prefix}user_info WHERE login_id = :login_id LIMIT 1";
+  $statement = $link->prepare($query);
+  $statement->bindParam('login_id', $login_id);
+  $statement->execute();
   
+  $result = $statement->fetch(PDO::FETCH_ASSOC);
+  return array_merge($session, $result);
 }
 
 ?>
--- login.php
+++ login.php
@@ -2,7 +2,19 @@
 namespace YABA;
 
 require_once('includes/user.inc.php');
+require_once('includes/files.inc.php');
+
+$error = false;
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-  
+  if(login($_POST['email_address'], $_POST['password'])) {
+    $_SESSION = get_session_array();
+  }
+  else {
+    $error = true;
+    $error_msg = "Invalid username and/or password";
+    load_page('login');
+  }
+} else {
+  load_page('login');
 }
