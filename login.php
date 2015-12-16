678e3ee0959f1933daf8f0afb6d4efd136cbde09
Jason Frost <jasonf@hotmail.com.au>
2015-12-17 01:25:49 +1100

    defined $error_msg at the top of the file and added redirect once login is successful

--- login.php
+++ login.php
@@ -5,10 +5,12 @@ require_once('includes/user.inc.php');
 require_once('includes/files.inc.php');
 
 $error = false;
+$error_msg = "";
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
   if(login($_POST['email_address'], $_POST['password'])) {
     $_SESSION = get_session_array();
+    header('Location: index.php');
   }
   else {
     $error = true;
