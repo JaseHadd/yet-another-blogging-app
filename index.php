954a329e7b7c1084446b86c26e01c5dc0b9ef1e6
Jason Frost <jasonf@hotmail.com.au>
2015-12-16 17:29:01 +1100

    made basic layout for testing

--- index.php
+++ index.php
@@ -2,8 +2,17 @@
 namespace YABA;
 
 if(!file_exists('config/')) {
-  header("Location: setup/index.php?page=1");
+  header('Location: setup/index.php?page=1');
 }
 
-require_once('db.inc.php');
+require_once('includes/pdo.inc.php');
+require_once('includes/files.inc.php');
+
+$config = load_file('blog');
+
+require('pages/header.html');
+require('pages/main.html');
+require('pages/sidebar.html');
+require('pages/footer.html');
+
 ?>
