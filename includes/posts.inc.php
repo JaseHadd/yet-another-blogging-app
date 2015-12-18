06c1ff37dafe0578dda600dbf7e8a55ab16ebcf8
Jason Frost <jasonf@hotmail.com.au>
2015-12-18 13:34:26 +1100

    fixed error in post sql

--- includes/posts.inc.php
+++ includes/posts.inc.php
@@ -9,13 +9,13 @@ function get_posts($number, $from) {
   $config = load_object('db');
   $link = database_connect();
   
-  $query = "SELECT title, post FROM {$config->prefix}posts
+  $query = "SELECT title, body FROM {$config->prefix}posts
               LIMIT :limit OFFSET :offset";
   $statement = $link->prepare($query);
   $statement->bindParam('limit', $number, PDO::PARAM_INT);
   $statement->bindParam('offset', $from, PDO::PARAM_INT);
   $statement->execute();
   
-  $results = $statement->fetchAll();
+  $results = $statement->fetchAll(PDO::FETCH_OBJ);
   return $results;
 }
