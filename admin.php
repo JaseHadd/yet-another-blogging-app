936cf1350495a01a162a8fe6e68420ebbbdae99b
Jason Frost <jasonf@hotmail.com.au>
2015-12-20 02:07:08 +1100

    added session_start to admin.php

--- admin.php
+++ admin.php
@@ -3,11 +3,12 @@ namespace YABA;
 
 require_once('includes/includes.inc.php');
 
+session_start();
+
 if(!logged_in()){
   header('Location: index.php');
   exit();
 }
-
 if($_SERVER['REQUEST_TYPE'] == "GET") {
   load_page('newpost');
 } else {
