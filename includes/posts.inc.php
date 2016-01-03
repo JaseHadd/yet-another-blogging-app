ba54c6ad75b2c5445126b01b90167c3faaf7c781
Jason Frost <jasonf@hotmail.com.au>
2016-01-04 02:43:13 +1100

    changed insert query for posts to set the created/modified time

--- includes/posts.inc.php
+++ includes/posts.inc.php
@@ -2,6 +2,8 @@
 namespace YABA;
 use \PDO;
 
+define('DB_DATE_FORMAT', 'Y-m-d H:i:s');
+
 require_once('includes/includes.inc.php');
 
 function get_posts($number, $from) {
@@ -24,13 +26,14 @@ function add_post($title, $text, $author, $category) {
   $link = database_connect();
   
   $query = "INSERT INTO {$config->prefix}posts
-              (title, body, author_id, category_id)
-              VALUES (:title, :body, :author, :category)";
+              (title, body, author_id, category_id, created_time, modified_time)
+              VALUES (:title, :body, :author, :category, :created, :created)";
   $statement = $link->prepare($query);
   $statement->bindParam('title', $title, PDO::PARAM_STR);
   $statement->bindParam('body', $text, PDO::PARAM_STR);
   $statement->bindParam('author', $author, PDO::PARAM_INT);
   $statement->bindParam('category', $category, PDO::PARAM_INT);
+  $statement->bindParam('created', gmdate(DB_DATE_FORMAT));
   
   $statement->execute();
 }
