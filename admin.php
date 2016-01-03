a745f8151bce612ed534c6fc5c7ead9e6dc692b0
Jason Frost <jasonf@hotmail.com.au>
2016-01-04 01:50:48 +1100

    updated admin script

--- admin.php
+++ admin.php
@@ -9,10 +9,21 @@ if(!logged_in()){
   header('Location: index.php');
   exit();
 }
+$pages = [
+  'newpost',
+];
+$page = array_key_exists('page', $_GET)?$_GET['page']:'newpost';
 
 if($_SERVER['REQUEST_METHOD'] == "GET") {
-  load_page('newpost');
+  load_page($page);
 } else {
-  /* post stuff here */
+  call_user_func($page);
 }
+
+function newpost() {
+  $title = $_POST['post_title'];
+  $text = $_POST['post_text'];
+  $category = $_POST['post_category'];
+}
+
 ?>
