30fb382ecf79dc4d533f56d6d5ea7a68ecacf624
Jason Frost <jasonf@hotmail.com.au>
2016-01-04 01:50:08 +1100

    added add_post function

--- includes/posts.inc.php
+++ includes/posts.inc.php
@@ -18,3 +18,19 @@ function get_posts($number, $from) {
   $results = $statement->fetchAll(PDO::FETCH_OBJ);
   return $results;
 }
+
+function add_post($title, $text, $author, $category) {
+  $config = load_object('db');
+  $link = database_connect();
+  
+  $query = "INSERT INTO {$config->prefix}posts
+              (title, body, author_id, category_id)
+              VALUES (:title, :body, :author, :category)";
+  $statement = $link->prepare($query);
+  $statement->bindParam('title', $title, PDO::PARAM_STR);
+  $statement->bindParam('body', $text, PDO::PARAM_STR);
+  $statement->bindParam('author', $author, PDO::PARAM_INT);
+  $statement->bindParam('category', $category, PDO::PARAM_INT);
+  
+  $statement->execute();
+}
