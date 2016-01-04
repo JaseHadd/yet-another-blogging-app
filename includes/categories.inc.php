627a0c8573512c3e12a1db009f267d389416854f
Jason Frost <jasonf@hotmail.com.au>
2016-01-04 17:27:58 +1100

    added category_name

--- includes/categories.inc.php
+++ includes/categories.inc.php
@@ -9,9 +9,17 @@ function get_categories() {
   $link = database_connect();
   $query = "SELECT category_id, name FROM {$config->prefix}categories";
   $statement = $link->prepare($query);
-  $statement->bindParam('email', $email_address);
   $statement->execute();
   
   $result = $statement->fetchAll(PDO::FETCH_OBJ);
   return $result;
+  
+function category_name($id) {
+  $config = load_object('db');
+  $link = database_connect();
+  $query = "SELECT name FROM {$config->prefix}categories
+              WHERE category_id = :id";
+  $statement = $link->prepare($query);
+  $statement->bind_param('id', $id, PDO::PARAM_INT);
+  return $statement->fetchColumn();
 }
