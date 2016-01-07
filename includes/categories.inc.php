730525805968eedd0d581e2f00062b979ed15294
Jason Frost <jasonf@hotmail.com.au>
2016-01-07 20:34:50 +1100

    testing category tree

--- includes/categories.inc.php
+++ includes/categories.inc.php
@@ -7,12 +7,34 @@ require_once('includes/includes.inc.php');
 function get_categories() {
   $config = load_object('db');
   $link = database_connect();
-  $query = "SELECT category_id, name FROM {$config->prefix}categories";
+  $query = "SELECT category_id, name, parent_id FROM {$config->prefix}categories";
   $statement = $link->prepare($query);
   $statement->execute();
   
   $result = $statement->fetchAll(PDO::FETCH_OBJ);
   return $result;
+}
+
+function get_category_tree() {
+  $categories = get_categories();
+  $tree = [];
+  while(count($categories) != 0) {
+    foreach($categories as $index => $category) {
+      $category->children = [];
+      if($category->parent_id = NULL) {
+        $tree[] = $category;
+        unset($categories[$index]);
+      }
+      else {
+        foreach($tree as $parent) {
+          if($parent->category_id == $category->parent_id) {
+            $parent->children[] = $category;
+            unset($categories[$index]);
+          }
+        }
+      }
+    }
+  }
 }
   
 function category_name($id) {
